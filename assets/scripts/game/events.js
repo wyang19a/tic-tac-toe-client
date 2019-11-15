aa5a32051a767c2b7a6ba59638a76a7988ef382d
wyang19a <wyang19a@gmail.com>
2019-11-14 22:22:02 -0500

    Little progress on game logic
    got it to mark x and o in turn on click, and disabled choosing already chosen grid.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,38 +1,37 @@
 'use strict'
 // const ui = require('./ui')
+// const currentPlayer = 'X'
 
-const emptyBoard = ['', '', '', '', '', '', '', '', '']
-for (let i = 0; i < emptyBoard.length; i++) {
-  if (i % 2 === 0) {
-    // console.log('current player is X')
-  } else {
-    // console.log('current player is O')
+// const emptyBoard = ['', '', '', '', '', '', '', '', '']
+
+// assign grid to emptyBoard[i]
+// push player.player_x or player.player_o to emptyBoard[i] by clicking on a grid
+
+let currentPlayer = 'X'
+const toggleTurn = () => {
+  if (currentPlayer === 'X') {
+    currentPlayer = 'O'
+  } else if (currentPlayer === 'O') {
+    currentPlayer = 'X'
   }
 }
-
 // listen for click, push X or O only if there's no string inside.
 const onPlayMove = event => {
-  // const value = () => {
-  //   // let currentValue
-  // for (let i = 1; i <= emptyBoard.length; i++) {
-  //   console.log(i)
-  // if (i % 2 === 0) {
-  //   currentValue = 'O'
-  // } else if (i % 2 === 1) {
-  //   currentValue = 'X'
-  // }
-  // return currentValue
-  // }
-  const clickOnGrid = event.target // return id of grid when clicked.
+  const clickOnGrid = $(event.target)
   const putValue = () => {
-    $(clickOnGrid).html('X')
+    // if there is no value inside clicked box, run toggleTurn() and pass in currentPlayer
+    if (clickOnGrid.html() === '') {
+      clickOnGrid.html(currentPlayer, toggleTurn())
+      // else, if there's any html, do nothing and just console log for now.
+    } else {
+      console.log('already selected')
+    }
   }
-  return putValue()
+  putValue()
 }
-// console.log(clickOnGrid)
-// for (let i = 0; i < gameBoard.legnth; i++) {
-// if (gameBoard[i] === '') {
-//   gameBoard.push('X')
+// check for winner
+// const checkForWinner = () => {
+//
 // }
 
 const addHandlers = event => {
@@ -42,3 +41,16 @@ const addHandlers = event => {
 module.exports = {
   addHandlers
 }
+
+//
+// Spencer's idea:
+// assign id's 0 to 8 to html grid, get a click to pull up id by event.click.id
+// and turn it into integer with jquery. push it up using emptyBoard[i]
+//
+// Derek's:
+// let current payer be true.
+// using if statement and !currentPlayer, proceed or not.
+//
+// Tory's:
+// currentPlayer='X
+//
