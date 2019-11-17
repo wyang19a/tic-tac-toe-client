b7161e43eb92becf9714e35ee7d2b1954a4219f2
wyang19a <wyang19a@gmail.com>
2019-11-17 14:40:10 -0500

    Edited events.js comments

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -16,10 +16,12 @@ const onNewGame = () => {
 let currentPlayer = 'X'
 let moveNum = 0
 const toggleTurn = () => {
+  // if current player is X, change current player to O and add number of move.
   if (currentPlayer === 'X') {
     currentPlayer = 'O'
     moveNum += 1
     return ui.onMoveSuccess('Turn: O')
+  // if current player is O, change current player to X and add number of move.
   } else if (currentPlayer === 'O') {
     currentPlayer = 'X'
     moveNum += 1
@@ -39,6 +41,7 @@ const onPlayMove = event => {
       clickOnGrid.html(currentPlayer, toggleTurn())
     }
     const storedCell = store.game.cells
+    // check for winner
     const checkForWin = () => {
       if ((storedCell[0] === 'X' && storedCell[1] === 'X' && storedCell[2] === 'X') ||
       (storedCell[0] === 'X' && storedCell[3] === 'X' && storedCell[6] === 'X') ||
@@ -48,6 +51,7 @@ const onPlayMove = event => {
       (storedCell[2] === 'X' && storedCell[5] === 'X' && storedCell[8] === 'X') ||
       (storedCell[3] === 'X' && storedCell[4] === 'X' && storedCell[5] === 'X') ||
       (storedCell[6] === 'X' && storedCell[7] === 'X' && storedCell[8] === 'X')) {
+        // reset game move to 0, set game over, and display winner X
         moveNum = 0
         store.game.over = true
         currentPlayer = 'X'
@@ -60,6 +64,7 @@ const onPlayMove = event => {
       (storedCell[2] === 'O' && storedCell[5] === 'O' && storedCell[8] === 'O') ||
       (storedCell[3] === 'O' && storedCell[4] === 'O' && storedCell[5] === 'O') ||
       (storedCell[6] === 'O' && storedCell[7] === 'O' && storedCell[8] === 'O')) {
+        // reset game move to 0, set game over, and display winner O
         moveNum = 0
         store.game.over = true
         currentPlayer = 'X'
@@ -67,21 +72,22 @@ const onPlayMove = event => {
       }
     }
     checkForWin()
-    console.log(store.game.cells)
-    console.log(store.game)
-    // }))
-    console.log(moveNum)
+    // console.log(store.game.cells)
+    // console.log(store.game)
+    // console.log(moveNum)
+    //
+    // game resets current player and displays it is a tied game after 9 moves.
     if (moveNum === 9) {
       moveNum = 0
       currentPlayer = 'X'
       store.game.over = true
       return ui.onDraw("It's a tie")
     }
+    // resets stored game info when game is over.
     if (store.game.over === true) {
       store.game = {}
       // return ui.onGameFinish('Click start over to play again!')
     }
-    // console.log($.inArray('X', store.game.cells))
   }
   putValue()
   // console.log(clickOnGrid)
